5278e306388b34625b3c1db8d75d74ae7c3821c8
Maximilian Schwaerzler <maschwaerzler@a1.net>
2025-03-08 21:03:47 +0100

    Remove TODO

--- app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/db/Facility.kt
+++ app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/db/Facility.kt
@@ -1,6 +1,5 @@
 package com.github.maximilianschwaerzler.ethuzhmensa.data.db
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -9,5 +8,4 @@ data class Facility(
     @PrimaryKey val facilityId: Int,
     val name: String,
     val location: String,
-    // TODO: val customerGroups: HashMap<Int, String>
 )

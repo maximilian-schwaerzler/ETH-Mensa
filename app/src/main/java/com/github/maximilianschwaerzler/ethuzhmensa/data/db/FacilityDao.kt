41aa0b8f9f17e4b4dc837ad3b0ab022f218c36d6
Maximilian Schwaerzler <maschwaerzler@a1.net>
2025-03-14 20:50:47 +0100

    Refactor DailyOffer and LocalDailyMenu to use Price value class for monetary values

--- app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/db/FacilityDao.kt
+++ app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/db/FacilityDao.kt
@@ -3,7 +3,6 @@ package com.github.maximilianschwaerzler.ethuzhmensa.data.db
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
-import androidx.room.Transaction
 import androidx.room.Upsert
 import com.github.maximilianschwaerzler.ethuzhmensa.data.db.entities.Facility
 import kotlinx.coroutines.flow.Flow

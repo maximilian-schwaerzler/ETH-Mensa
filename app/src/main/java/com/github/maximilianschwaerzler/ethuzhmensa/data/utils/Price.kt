6e50be3f946e8a9b4987138aa09e20a5fce56f20
Maximilian Schwaerzler <maschwaerzler@a1.net>
2025-03-14 20:50:21 +0100

    Add Price value class to handle monetary values with precision

---
+++ app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/utils/Price.kt
@@ -0,0 +1,28 @@
+package com.github.maximilianschwaerzler.ethuzhmensa.data.utils
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+/**
+ * Represents a price with at most 2 decimal places.
+ */
+@JvmInline
+value class Price(private val amount: BigDecimal) : Comparable<Price> {
+
+    init {
+        require(amount.scale() <= 2) { "Price must have at most 2 decimal places." }
+    }
+
+    constructor(value: String) : this(BigDecimal(value).setScale(2, RoundingMode.HALF_UP))
+    constructor(value: Double) : this(BigDecimal(value).setScale(2, RoundingMode.HALF_UP))
+    constructor(value: Int) : this(BigDecimal(value).setScale(2, RoundingMode.UNNECESSARY))
+
+    operator fun plus(other: Price): Price = Price(amount + other.amount)
+    operator fun minus(other: Price): Price = Price(amount - other.amount)
+    operator fun times(multiplier: BigDecimal): Price = Price(amount.multiply(multiplier).setScale(2, RoundingMode.HALF_UP))
+    operator fun div(divisor: BigDecimal): Price = Price(amount.divide(divisor, 2, RoundingMode.HALF_UP))
+
+    override fun compareTo(other: Price): Int = amount.compareTo(other.amount)
+
+    fun toBigDecimal(): BigDecimal = amount
+}

5b8f534eaf9f32163b4656323145a29adc36aeb5
Maximilian Schwaerzler <maschwaerzler@a1.net>
2025-03-16 19:35:15 +0100

    chore(build): Update versionCode to 20 and versionName to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.maximilianschwaerzler.ethuzhmensa"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.1.0"
+        versionCode = 20
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

72300ee22e0bed13f65f5ac4c29506f908d704e8
Maximilian Schwaerzler <maschwaerzler@a1.net>
2025-03-25 11:24:57 +0100

    feat(overview): enhance refreshData with error handling for network issues

--- app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/OverviewScreenViewModel.kt
+++ app/src/main/java/com/github/maximilianschwaerzler/ethuzhmensa/data/OverviewScreenViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.maximilianschwaerzler.ethuzhmensa.data.db.entities.Facility
 import com.github.maximilianschwaerzler.ethuzhmensa.data.db.entities.OfferWithPrices
-import com.github.maximilianschwaerzler.ethuzhmensa.repository.FacilityRepository2
-import com.github.maximilianschwaerzler.ethuzhmensa.repository.MenuRepository2
+import com.github.maximilianschwaerzler.ethuzhmensa.repository.FacilityRepository
+import com.github.maximilianschwaerzler.ethuzhmensa.repository.MenuRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,13 +19,14 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
+import java.net.UnknownHostException
 import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
 class OverviewScreenViewModel @Inject constructor(
-    private val facilityInfoRepo: FacilityRepository2,
-    private val menuRepository: MenuRepository2,
+    private val facilityInfoRepo: FacilityRepository,
+    private val menuRepository: MenuRepository,
 ) : ViewModel() {
 
     private val offers = MutableStateFlow<List<OfferWithPrices>>(emptyList())
@@ -40,46 +43,29 @@ class OverviewScreenViewModel @Inject constructor(
         .onStart { refreshData() }
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), listOf())
 
-    fun refreshData() = viewModelScope.launch(Dispatchers.IO) {
-        _isLoading.emit(true)
-        supervisorScope {
-            launch {
-                try {
-                    facilities.emit(facilityInfoRepo.getAllFacilities())
-                } catch (e: IllegalStateException) {
-                    Log.d("OverviewScreenViewModel", "Crash!", e)
+    fun refreshData() =
+        viewModelScope.launch(Dispatchers.IO + CoroutineExceptionHandler { coroutineContext, throwable ->
+            if (throwable is UnknownHostException) {
+                Log.d("OverviewScreenViewModel", "No internet connection", throwable)
+                // TODO: Show error message to user
+                coroutineContext.cancel()
+            } else {
+                Log.d("OverviewScreenViewModel", "Crash!", throwable)
+            }
+        }) {
+            _isLoading.emit(true)
+            supervisorScope {
+                launch {
+                    try {
+                        facilities.emit(facilityInfoRepo.getAllFacilities())
+                    } catch (e: IllegalStateException) {
+                        Log.d("OverviewScreenViewModel", "Crash!", e)
+                    }
+                }
+                launch {
+                    offers.emit(menuRepository.getOffersForDate(LocalDate.now()))
                 }
             }
-            launch {
-                offers.emit(menuRepository.getOffersForDate(LocalDate.now()))
-            }
+            _isLoading.emit(false)
         }
-        _isLoading.emit(false)
-    }
-
-//    suspend fun shouldUpdateFacilityInfo(): Boolean {
-//        val lastFacilityInfoUpdate = dataStoreManager.lastFacilityFetchDate.firstOrNull()
-//        if (lastFacilityInfoUpdate == LocalDate.MIN || lastFacilityInfoUpdate == null) {
-//            Log.d(
-//                "OverviewScreenViewModel",
-//                "No facility info update time found, fetching data from the net"
-//            )
-//            return true
-//        } else if (LocalDate.now().minusDays(
-//                appContext.resources.getInteger(R.integer.config_facility_info_stale_days)
-//                    .toLong()
-//            ).isAfter(lastFacilityInfoUpdate as ChronoLocalDate?)
-//        ) {
-//            Log.d(
-//                "OverviewScreenViewModel",
-//                "Facility info is older than 7 days, fetching data from the net"
-//            )
-//            return true
-//        } else if (facilities.first().isEmpty()) {
-//            Log.d("OverviewScreenViewModel", "No facility info found, fetching data from the net")
-//            return true
-//        }
-//        Log.d("OverviewScreenViewModel", "Facility info is up to date")
-//        return false
-//    }
 }
